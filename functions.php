2d3c91ad4c1ec6f104adf5cddd0df28d1d71b2d6
RobertoFloris <florisroberto99@gmail.com>
2025-03-27 10:34:11 +0100

    [fix] structure and completed milestone 3 and 4

--- functions.php
+++ functions.php
@@ -1,11 +1,33 @@
-<?php
-function randomPassword($lengthPass) {
-  $alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{};:,.?/";
-  $pass = "";
-  for ($i = 0; $i < $lengthPass; $i++) {
-      $n = rand(0, strlen($alphabet) - 1); 
-      $pass .= $alphabet[$n]; 
-  }
-  return $pass;
-}
+<?php 
+ $pass = ""; 
+ if(isset($_GET['num'])){
+
+   $alphabetLowers = "abcdefghijklmnopqrstuvwxyz";
+   $alphabetUppers= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+   $numbers = "0123456789";
+   $symbol = "!@#$%^&*()-_=+[]{};:,.?/";
+
+   $alphabet= "";
+
+   if(isset($_GET['lower']) && isset($_GET['lower']) == "on"){
+    $alphabet .= $alphabetLowers;
+   }
+
+   if(isset($_GET['upper']) && isset($_GET['upper']) == "on"){
+    $alphabet .= $alphabetUppers;
+   }
+
+   if(isset($_GET['number']) && isset($_GET['number']) == "on"){
+    $alphabet .= $numbers;
+   }
+
+   if(isset($_GET['symbol']) && isset($_GET['symbol']) == "on"){
+    $alphabet .= $symbol;
+   }
+
+   for ($i = 0; $i < $_GET['num']; $i++) {
+       $n = rand(0, strlen($alphabet) - 1); 
+       $pass .= $alphabet[$n]; 
+   }
+ }
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-<?php 
-require_once "./functions.php"
+<?php
+ require_once "./functions.php";
+ if($pass != ""){
+  session_start();
+
+  $_SESSION['password'] = $pass;
+
+  header("Location: ./result.php");
+ }
 ?>
 
 <!DOCTYPE html>
@@ -7,20 +14,29 @@ require_once "./functions.php"
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Generatore di Password</title>
 </head>
 <body>
-  <form action="" method="get">
-    <label for="num">quanti caratteri per la tua password?</label>
+  <form action="" method="GET">
+    <label for="num">Quanti caratteri per la tua password?</label>
     <input type="number" name="num">
-    <button>Invia</button>
-  </form>
 
-  <?php 
-    if (isset($_GET['num']) && $_GET['num'] !== "") { 
-      echo "<p> La tua password consigliata di " . $_GET['num'] . " caratteri è: " . randomPassword($_GET['num']) . "</p>";
-  }
-  ?>
+    <br>
+    <label for="lower">Lettere minuscole?</label>
+    <input type="checkbox" name="lower" id="lower">
+
+    <label for="upper">Lettere maiuscole?</label>
+    <input type="checkbox" name="upper" id="upper">
+
+    <label for="number">Numeri?</label>
+    <input type="checkbox" name="number" id="number">
+
+    <label for="symbol">Simboli?</label>
+    <input type="checkbox" name="symbol" id="symbol">
+
+    <br>
+    <button type="submit">Invia</button>
+  </form>
 
 </body>
 </html>
